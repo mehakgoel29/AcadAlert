804de4dc5420738bab53eca21c1961413df8c15b
Mehak Goel <mhkgoel29@gmail.com>
2025-08-01 10:56:52 +0530

    user feed

--- ENoticeBoard/NoticeApp/urls.py
+++ ENoticeBoard/NoticeApp/urls.py
@@ -1,14 +1,28 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # Authentication
     path('', views.custom_login_view, name='login'),
     path('logout/', views.custom_logout_view, name='logout'),
 
+   # Forgot Password URLs
+    path('password_reset/', 
+         auth_views.PasswordResetView.as_view(template_name='password_reset.html'), 
+         name='password_reset'),
+    path('password_reset_done/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), 
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), 
+         name='password_reset_confirm'),
+    path('reset_done/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), 
+         name='password_reset_complete'),
     # User views
     path('user-dashboard/', views.user_dashboard, name='user_dashboard'),
-    path('create-blog/', views.create_blog, name='create_blog'),
+    path('create-notice/', views.create_blog, name='create_blog'),
     path('my-blogs/', views.my_blogs, name='my_blogs'),
     path('user-feed/', views.user_feed, name='user_feed'),
 
